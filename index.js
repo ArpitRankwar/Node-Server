7de1ecb86d53b9169ccddc9469a70dc44bfb940f
mohitraj610 <mohitraj1999@gmail.com>
2022-04-23 16:46:10 +0530

    V3

--- index.js
+++ index.js
@@ -13,9 +13,28 @@ connection();
 app.use(express.json());
 app.use(cors());
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '.');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 app.listen(port, console.log(`Listening on port ${port}...`));
